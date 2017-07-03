c68aa6c5025bd9d3fba4bd1aaee0ae610fba47ed
DimuthRuwantha <dimuthruwantha@gmail.com>
2017-07-04 01:29:34 +0530

    code refactor

--- src/main/java/com/ruh/eng/Assignment.java
+++ src/main/java/com/ruh/eng/Assignment.java
@@ -1,13 +1,13 @@
 package com.ruh.eng;
 
 import com.ruh.eng.Loggers.LogMessage;
-import com.ruh.eng.dataImport.FileReaderFactory;
 import com.ruh.eng.dataImport.FilePath;
-import com.ruh.eng.databse.dataaccess.connectors.JdbcConnector;
+import com.ruh.eng.dataImport.FileReaderFactory;
 import com.ruh.eng.databse.dataaccess.dao.FactoryDAO;
 import com.ruh.eng.exceptions.CsvFormatException;
 import com.ruh.eng.mapper.ObjectMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,7 +25,7 @@ public class Assignment
 		FileReaderFactory fileReaderFactory = new FileReaderFactory();  // Setting the file path to the folder. this can be set in any where
 		FilePath filePath = new FilePath();
 		FactoryDAO factoryDAO = new FactoryDAO();
-		List<List<String>> dataList = null;
+		List<List<String>> dataList = new ArrayList<>(  );
 		
 		filePath.setFilePath( "D:/Academics/7th Semester/EE7205 OOP Design Patterns & Principles/csv files/users.csv" );
 		
@@ -37,14 +37,15 @@ public class Assignment
 		catch ( CsvFormatException e ) {
 			Logger.getLogger( Assignment.class.getName() ).log( Level.WARNING, "CSV Format Exception", e );
 		}
-		if ( dataList != null ) {
+		if ( !dataList.isEmpty() ) {
 			ObjectMapper objectMapper = new ObjectMapper();
 			List<Object> objectList = objectMapper.MappedObject( dataList );
 			factoryDAO.insertDataFactory( objectList, dataList.get( 0 ) );
 		}
 		else {
 			LogMessage.out( "File is empty or not exist" );
+			Logger.getLogger( Assignment.class.getName() ).log( Level.CONFIG,"make sure file path is set" );
 		}
-		JdbcConnector.closeConnection();
+		
 	}
 }
--- src/main/java/com/ruh/eng/dataImport/FileReaderFactory.java
+++ src/main/java/com/ruh/eng/dataImport/FileReaderFactory.java
@@ -17,18 +17,23 @@ public class FileReaderFactory
 	private List<List<String>> dataList = new ArrayList<>();
 	
 	public List<List<String>> FileReader( String filename ) throws CsvFormatException {
-		IReader reader;
 		
+		IReader reader;
+		if ( filename == null ) {
+			return dataList;
+		}
+		else {
 			if ( filename.contains( ".csv" ) ) {    // for csv files
 				LogMessage.out( "File was identified as a csv file" );
 				reader = new CSVFileReader();
 				dataList = reader.fileReader( filename );
-				}
+			}
 			else if ( filename.contains( ".xlsx" ) ) {  // for excel sheets
 				LogMessage.out( "File was identified as a excel file" );
 				reader = new EXELFileReader();
 				dataList = reader.fileReader( filename );
 			}
+		}
 		return dataList;
 	}
 }
